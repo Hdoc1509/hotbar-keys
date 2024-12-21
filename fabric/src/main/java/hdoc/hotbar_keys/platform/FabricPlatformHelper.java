2a019663c9f48fa93fd2db57aaf805e5a8cdd2aa
hdoc <hector.ochoa.dev@gmail.com>
2024-12-21 13:02:39 -0500

    chore: setup up basic structure and metadata

---
+++ fabric/src/main/java/hdoc/hotbar_keys/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package hdoc.hotbar_keys.platform;
+
+import hdoc.hotbar_keys.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
