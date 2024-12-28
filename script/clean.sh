885f22a4835808442f16dca706792872204bcc9e
hdoc <hector.ochoa.dev@gmail.com>
2024-12-28 18:30:31 -0500

    fix(scripts): clean cache and metadate files correctly

--- script/clean.sh
+++ script/clean.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+
 _repo_root=$(git rev-parse --show-toplevel)
 
 # folders
-rm -r "$_repo_root"/**/bin
-rm -r "$_repo_root"/**/.settings
-rm -r "$_repo_root"/**/.gradle
-rm -r "$_repo_root"/**/build
+rm -r "$_repo_root"/{common,fabric}/bin
+rm -r "$_repo_root"/{.,common,fabric}/.settings
+rm -r "$_repo_root"/{.,buildSrc,fabric}/.gradle
+rm -r "$_repo_root"/{buildSrc,common,fabric}/build
 
 # files
-rm "$_repo_root"/**/.factorypath
-rm "$_repo_root"/**/.classpath
-rm "$_repo_root"/**/.project
+rm "$_repo_root"/{common,fabric}/.factorypath
+rm "$_repo_root"/{common,fabric}/.classpath
+rm "$_repo_root"/{.,common,fabric}/.project
