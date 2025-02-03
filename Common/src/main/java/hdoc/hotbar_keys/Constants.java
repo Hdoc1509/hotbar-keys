452aa4bafd415fc0270ff1c8d195fd570848df69
hdoc <hector.ochoa.dev@gmail.com>
2025-02-02 20:19:20 -0500

    feat(common): update metadata in Constants.java

--- Common/src/main/java/hdoc/hotbar_keys/Constants.java
+++ Common/src/main/java/hdoc/hotbar_keys/Constants.java
@@ -4,8 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Constants {
-
-    public static final String MOD_ID = "multiloader";
-    public static final String MOD_NAME = "Multi Loader Template";
+    // NOTE: should match the `mod_id` in `gradle.properties`
+    public static final String MOD_ID = "hotbar-keys";
+    public static final String MOD_NAME = "Hotbar Keys";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
