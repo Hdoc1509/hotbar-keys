c75eb0d9f3ea206fbdc925c33c831f12a26c2763
hdoc <hector.ochoa.dev@gmail.com>
2025-02-02 20:26:21 -0500

    refactor(common): simplify `init()` method of `CommonClass`

--- Common/src/main/java/hdoc/hotbar_keys/CommonClass.java
+++ Common/src/main/java/hdoc/hotbar_keys/CommonClass.java
@@ -2,12 +2,10 @@ package hdoc.hotbar_keys;
 
 import hdoc.hotbar_keys.platform.Services;
 
-import net.minecraft.core.Registry;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
 
 import java.util.List;
@@ -18,12 +16,9 @@ public class CommonClass {
     // game has no mechanism to load tooltip listeners so this must be
     // invoked from a mod loader specific project like Forge or Fabric.
     public static void init() {
-
-        Constants.LOG.info(
-                "Hello from Common init on {}! we are currently in a {} environment!",
-                Services.PLATFORM.getPlatformName(),
-                Services.PLATFORM.isDevelopmentEnvironment() ? "development" : "production");
-        Constants.LOG.info("Diamond Item >> {}", Registry.ITEM.getKey(Items.DIAMOND));
+        if (Services.PLATFORM.isModLoaded(Constants.MOD_ID)) {
+            Constants.LOG.info("{} successfully loaded", Constants.MOD_NAME);
+        }
     }
 
     // This method serves as a hook to modify item tooltips. The vanilla game
