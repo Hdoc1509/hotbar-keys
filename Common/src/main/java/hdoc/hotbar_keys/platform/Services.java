4d6d91961732fdcccd1626999474167bc7b1cac6
hdoc <hector.ochoa.dev@gmail.com>
2025-02-02 19:00:10 -0500

    chore: update group in `Common` project

---
+++ Common/src/main/java/hdoc/hotbar_keys/platform/Services.java
@@ -0,0 +1,24 @@
+package hdoc.hotbar_keys.platform;
+
+import hdoc.hotbar_keys.Constants;
+import hdoc.hotbar_keys.platform.services.IPlatformHelper;
+
+import java.util.ServiceLoader;
+
+public class Services {
+
+    public static final IPlatformHelper PLATFORM = load(IPlatformHelper.class);
+
+    public static <T> T load(Class<T> clazz) {
+
+        final T loadedService =
+                ServiceLoader.load(clazz)
+                        .findFirst()
+                        .orElseThrow(
+                                () ->
+                                        new NullPointerException(
+                                                "Failed to load service for " + clazz.getName()));
+        Constants.LOG.debug("Loaded {} for service {}", loadedService, clazz);
+        return loadedService;
+    }
+}
--- Common/src/main/java/hdoc/hotbar_keys/platform/services/IPlatformHelper.java
+++ Common/src/main/java/hdoc/hotbar_keys/platform/services/IPlatformHelper.java
@@ -1,4 +1,4 @@
-package com.example.examplemod.platform.services;
+package hdoc.hotbar_keys.platform.services;
 
 public interface IPlatformHelper {
 
