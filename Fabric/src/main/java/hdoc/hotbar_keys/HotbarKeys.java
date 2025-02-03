3f05e73ed6c364fb5c3c612bf1c28eddf7d6d41b
hdoc <hector.ochoa.dev@gmail.com>
2025-02-02 21:58:14 -0500

    feat(fabric): register key bindings

--- Fabric/src/main/java/hdoc/hotbar_keys/HotbarKeys.java
+++ Fabric/src/main/java/hdoc/hotbar_keys/HotbarKeys.java
@@ -1,23 +1,21 @@
 package hdoc.hotbar_keys;
 
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 
-public class ExampleMod implements ModInitializer {
+public class HotbarKeys implements ClientModInitializer {
 
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
 
         // This method is invoked by the Fabric mod loader when it is ready
         // to load your mod. You can access Fabric and Common code in this
         // project.
 
         // Use Fabric to bootstrap the Common mod.
-        Constants.LOG.info("Hello Fabric world!");
-        CommonClass.init();
+        KeyBindingHelper.registerKeyBinding(CommonClass.usePrevItem);
+        KeyBindingHelper.registerKeyBinding(CommonClass.useNextItem);
 
-        // Some code like events require special initialization from the
-        // loader specific code.
-        ItemTooltipCallback.EVENT.register(CommonClass::onItemTooltip);
+        CommonClass.init();
     }
 }
